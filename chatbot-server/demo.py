29e8ebeec2157a42e33d5279ba8eb02704b9229a
Rakshith Singh <rakshithsinghgm@gmail.com>
2020-11-10 16:38:09 -0500

    Update demo.py

--- chatbot-server/demo.py
+++ chatbot-server/demo.py
@@ -8,11 +8,3 @@ def chatbot_response(dialouge):
 	trainer = UbuntuCorpusTrainer(chatbot)
 	response = chatbot.get_response(dialouge)
 	return response
-"""
-rsp = chatbot_response("Hey")
-print("Type of rsp was ",type(rsp))
-
-#print("Members of rsp are", inspect.getmembers(rsp))
-
-print(" Type of Resp.text ",type(rsp.text))
-"""
